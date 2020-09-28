eabfca61d8644487d0a7226a9fc6f3f155cbcad4
Igor <igor.souza1.bezerra@gmail.com>
2020-09-28 09:23:16 -0300

    first commit

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '05f1ac8aed41a032a50f5a64e729fda7',
+    expiresIn: '1h',
+  },
+};
---
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -0,0 +1,32 @@
+import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+import AppointmentRepository from '../repositories/AppointmentsRepository';
+
+import AppError from '@shared/errors/AppError';
+
+interface Request {
+  provider_id: string;
+  date: Date;
+}
+
+class CreateAppointmentService {
+  public async execute({ provider_id, date }: Request): Promise<Appointment> {
+    const appointmentsRepository = getCustomRepository(AppointmentRepository);
+
+    const appointmentDate =  startOfHour(date);
+    const findAppointmentInSameDate = await appointmentsRepository.findByDate(appointmentDate);
+
+    if(findAppointmentInSameDate) {
+      throw new AppError('This appointment is already booked');
+    }
+
+    const appointment = appointmentsRepository.create({ provider_id, date: appointmentDate });
+    await appointmentsRepository.save(appointment);
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
