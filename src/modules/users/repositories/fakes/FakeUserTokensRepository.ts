e1e8b7e135aed9f6fa37d2367d50de5470efd47e
Igor <igor.souza1.bezerra@gmail.com>
2020-10-04 12:19:27 -0300

    resetPassword

--- src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
+++ src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -11,11 +11,21 @@ class FakeUserTokensRepository implements IUserTokensRepository {
 
     Object.assign(userToken, {
       id: uuid(),
+      token: uuid(),
       user_id,
+      created_at: new Date(),
+      updated_at: new Date(),
     });
 
     this.userToken.push(userToken);
 
+    return userToken;
+  }
+
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = this.userToken.find(
+      findToken => findToken.token === token,
+    );
     return userToken;
   }
 }
